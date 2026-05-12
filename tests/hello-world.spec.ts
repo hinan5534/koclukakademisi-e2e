ddfdf4a05a75417c29b26ec434482d759bd31ef4
devopstraining3455 <awstraining3455@gmail.com>
2026-05-12 11:47:19 +0300

    chore: remove auto-generated hello-world spec

--- tests/hello-world.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-
-describe('Hello World Functionality', () => {
-    it('should return "Hello, World!"', () => {
-        const result = 'Hello, World!';
-        expect(result).to.equal('Hello, World!');
-    });
-});
